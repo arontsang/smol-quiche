4ab51789eacaa443fbdad5f587cfbb49e0edc7b7
Aron Tsang <arontsang@gmail.com>
2020-12-08 22:11:37 +0800

    W00t got first h3 request working

--- examples/test.rs
+++ examples/test.rs
@@ -34,7 +34,56 @@ async fn main_async<'a>(scheduler: &LocalExecutor<'a>) -> quiche::Result<()> {
 
 
 
-    let http_client = SmolHttp3Client::new(quic).await;
+    if let Ok(mut http_client) = SmolHttp3Client::new(&quic, &scheduler).await {
+        let req = vec![
+            quiche::h3::Header::new(":method", "GET"),
+            quiche::h3::Header::new(":scheme", "https"),
+            quiche::h3::Header::new(":authority", "quic.tech"),
+            quiche::h3::Header::new(":path", "/"),
+            quiche::h3::Header::new("user-agent", "quiche"),
+        ];
+    
+        let request = http_client.send_request(&req).await.unwrap();
+
+        scheduler.run(async{
+
+            loop {
+                let foo = request.wait_events().await;
+
+                match foo {
+                    Err(_) => panic!(""),
+                    Ok(quiche::h3::Event::Finished) => break,
+                    Ok(quiche::h3::Event::Headers {list: headers, has_body: _}) => {
+                        for header in headers {
+                            let name = quiche::h3::NameValue::name(&header);
+                            let value = quiche::h3::NameValue::value(&header);
+                            println!("{} : {}", name, value);
+                        }
+                    },
+                    Ok(quiche::h3::Event::Data) => {
+                        let mut buffer: [u8; 3000] = [0; 3000];
+                        match request.read_body(&mut buffer).await {
+                            Ok(length) => {
+                                let body = std::str::from_utf8(&buffer[0..length]).unwrap();
+                                println!("{}", body);
+                            },
+                            Err(quiche::h3::Error::Done) => (),
+                            Err(er) =>
+                                println!("{}", er)
+                        };
+                    }
+                    Ok(quiche::h3::Event::Datagram) => panic!("Datagram not supported."),
+                    Ok(quiche::h3::Event::GoAway) => break,
+                }
+
+                
+            }
+        }).await;
+        std::mem::drop(http_client);
+        std::mem::drop(quic);
+    }
+
+
 
     Ok(())
 }
